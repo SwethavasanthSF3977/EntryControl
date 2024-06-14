d969da7aaac93df0b6e21136e1235ddc58252966
Swetha <swetha.sf3977@gmail.com>
2024-06-14 16:17:23 +0530

    Added Sample

--- Autocomplete/MauiProgram.cs
+++ Autocomplete/MauiProgram.cs
@@ -1,13 +1,11 @@
 ﻿using Microsoft.Extensions.Logging;
-using Syncfusion.Maui.Core.Hosting;
 
 namespace Autocomplete {
     public static class MauiProgram {
         public static MauiApp CreateMauiApp() {
             var builder = MauiApp.CreateBuilder();
             builder
-                .UseMauiApp<App>()
-                .ConfigureSyncfusionCore()
+                .UseMauiApp<App>()                
                 .ConfigureFonts(fonts => {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
